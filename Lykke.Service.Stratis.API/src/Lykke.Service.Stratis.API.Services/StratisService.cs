a946ee5bbc41cc93571cca7ecf81e682ee37c018
Waqar Abdul Ghaffar <Waqar.Ghaffar@lykke.com>
2018-02-07 20:42:00 +0500

    /api/wallets api created
    /api/wallets created

---
+++ Lykke.Service.Stratis.API/src/Lykke.Service.Stratis.API.Services/StratisService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.Log;
+using Lykke.Service.Stratis.API.Core.Services;
+using NBitcoin;
+using NBitcoin.Policy;
+
+namespace Lykke.Service.Stratis.API.Services
+{
+    public class StratisService: IStratisService
+    {
+        private readonly ILog _log;
+        private readonly Network _network;
+
+        public StratisService(ILog log,string network)
+        {
+            _log = log;
+            _network = Network.GetNetwork(network);
+        }
+
+        public bool IsValidPrivateKey(string privateKey)
+        {
+            try
+            {
+                Key.Parse(privateKey, _network);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public string GetPrivateKey()
+        {
+            var key = new Key();
+
+            return key.GetWif(_network).ToString();
+        }
+
+        public string GetPublicAddress(string privateKey)
+        {
+            var wallet = new BitcoinSecret(privateKey);
+
+            return wallet.PubKey.GetAddress(_network).ToString();
+        }
+
+      
+
+    }
+}
---
+++ Lykke.Service.Stratis.API/src/Lykke.Service.Stratis.API/Models/WalletResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.Service.Stratis.API.Models
+{
+    public class WalletResponse
+    {
+        public string PrivateKey { get; set; }
+
+        public string PublicAddress { get; set; }
+    }
+}
