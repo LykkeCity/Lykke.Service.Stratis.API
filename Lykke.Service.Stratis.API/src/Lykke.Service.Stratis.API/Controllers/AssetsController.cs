a4855a3152a718f7b1f0132f2a1d7e4ac98794f5
Waqar Abdul Ghaffar <Waqar.Ghaffar@lykke.com>
2018-02-08 13:32:10 +0500

    /api/assets?take=integer&[continuation=string] service implemented
    /api/assets?take=integer&[continuation=string] service implemented

--- Lykke.Service.Stratis.API/src/Lykke.Service.Stratis.API/Controllers/AssetsController.cs
+++ Lykke.Service.Stratis.API/src/Lykke.Service.Stratis.API/Controllers/AssetsController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+using Lykke.Service.BlockchainApi.Contract;
 using Lykke.Service.BlockchainApi.Contract.Assets;
 using Lykke.Service.Stratis.API.Core;
 using Lykke.Service.Stratis.API.Helper;
@@ -16,6 +18,14 @@ namespace Lykke.Service.Stratis.API.Controllers
         public AssetResponse GetAsset(string assetId)
         {
             return Constants.Assets.TryGetValue(assetId, out var asset) ? asset.ToAssetResponse() : null;
+        }
+
+        [HttpGet]
+        public PaginationResponse<AssetResponse> Get([Required, FromQuery] int take, [FromQuery] string continuation)
+        {
+            var assets = new[] { Asset.Stratis.ToAssetResponse() };
+
+            return PaginationResponse.From("", assets);
         }
     }
 }
